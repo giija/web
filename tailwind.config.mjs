6c3f3e296fcb189a440a2ff7299fbeb256d66c1a
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-03-29 14:19:20 +0100

    feat: add more decoration to homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,9 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        brand: ["Outfit", "Arial", "sans-serif"],
-      },
-    },
+    extend: {},
     fontFamily: {
-      sans: ["DM Sans", "Inter", "Helvetica", "Arial", "sans-serif"],
+      sans: ["Outfit", "Inter", "Helvetica", "Arial", "sans-serif"],
     },
   },
   plugins: [],
